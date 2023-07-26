f88612d44ed86255db0c191d5b9ac8b3ebd23fc2
gogi999 <g.petrovic9@gmail.com>
2023-07-26 12:18:27 +0200

    App completed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import React, {
 } from 'react';
 
 import Error from './components/Error';
+import FinishScreen from './components/FinishScreen';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Loader from './components/Loader';
 import Main from './components/Main';
+import NextButton from './components/NextButton';
+import Progress from './components/Progress';
 import Question from './components/Question';
 import StartScreen from './components/StartScreen';
+import Timer from './components/Timer';
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = { 
   questions: [],
@@ -17,6 +24,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -36,6 +45,7 @@ function reducer(state, action) {
       return { 
         ...state, 
         status: 'active', 
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       }
     case 'newAnswer':
       const question = state.questions.at(state.index);
@@ -47,15 +57,32 @@ function reducer(state, action) {
           ? state.points + question.points 
           : state.points,
       }
+    case 'nextQuestion': 
+      return { ...state, index: state.index + 1, answer: null }
+    case 'finish':
+      return { 
+        ...state, 
+        status: 'finished', 
+        highscore: state.points > state.highscore ? state.points : state.highscore
+      }
+    case 'restart':
+        return { ...initialState, questions: state.questions, status: 'ready' };
+        case 'tick':
+          return { 
+            ...state, 
+            secondsRemaining: state.secondsRemaining - 1, 
+            status: state.secondsRemaining === 0 ? 'finished' : state.status
+          }
     default: 
       throw new Error('Unknown action!');
   }
 }
 
 const App = () => {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
 
   useEffect(() => {
     fetch('http://localhost:8000/questions')
@@ -67,17 +94,44 @@ const App = () => {
   return (
     <div className="app">
       <Header />
+
       <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' && (
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && (
-          <Question 
-            question={questions[index]} 
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
             dispatch={dispatch}
-            answer={answer}  
+            highscore={highscore}
           />
         )}
       </Main>
---
+++ src/components/Progress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Progress = ({ index, numQuestions, points, maxPossiblePoints, answer }) => {
+    return (
+        <header className="progress">
+            <progress value={index + Number(answer !== null)} max={numQuestions} />
+            <p>Question <strong>{index}</strong> / {numQuestions}</p>
+            <p><strong>{points}</strong> / {maxPossiblePoints}</p>
+        </header>
+    );
+}
+
+export default Progress;
